1f97835b846e132102bfd93231b9b98e51cd8d9e
SAURABH\Saurabh <saurabhkoshatwar1996@gmail.com>
2018-01-21 22:53:41 +0530

    ss

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def hello():
-    return "<h1>Hello</h1>"
+    return "<h1>Hello flask</h1>"
 
 @app.route('/api/v1/test/<int:id>', methods=['GET'])
 def tester(id):
