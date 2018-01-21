8b9370ecef87ea1f7a5e6d1d77be49b5ea322bde
Omkar Shelar <omkarshelar24@gmail.com>
2018-01-21 21:32:20 +0530

    Added basic DB connection

---
+++ dbconn.py
@@ -0,0 +1,22 @@
+import os
+from urllib import parse
+import psycopg2
+
+parse.uses_netloc.append("postgres")
+url = parse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+def dbtest(id):
+	cur = conn.cursor()
+	cur.execute('SELECT name from test WHERE id='+str(id))
+	rows = cur.fetchall()
+	print(rows)
+	for row in rows:
+		return row[0]
--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
 from flask import Flask
+import dbconn
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET'])
 def hello():
-    return "<h6>Hello World!</h6>"
+    return "<h1>Hello World!</h1>"
 
+@app.route('/api/v1/test/<int:id>', methods=['GET'])
+def tester(id):
+	return(dbconn.dbtest(id))
 
 if __name__ == '__main__':
     app.run()
